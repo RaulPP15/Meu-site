5b0b789be1b53b8ec027eca8d4643e1bea31cad9
RaulPP15 <raulpachecopereira@outlook.com>
2025-08-03 17:21:57 -0300

    atualização do trabalho de arte

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function video() {
+function escondertexto() {
     document.getElementById("textos").style.display = "none";
+}
+function mostrartexto() {
+    document.getElementById("textos").style.display = "block";
+}
+function escondervideo() {
+    document.getElementById("video").style.display = "none";
+}
+function mostrarvideo() {
+    document.getElementById("video").style.display = "block";
+}
+function esconderpptx() {
+    document.getElementById("pptx").style.display = "none";
+}
+function mostrarpptx() {
+    document.getElementById("pptx").style.display = "block";
 }
